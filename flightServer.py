f4c425d98506559bb56d66335e211fb2b98ceb4b
Sukrit Goyal <85162845+sukritS009312@users.noreply.github.com>
2021-06-09 15:23:20 +0530

    Update flightServer.py

--- flightServer.py
+++ flightServer.py
@@ -69,7 +69,7 @@ while True:
                 print("-"*80)
                 print("Flight No: "+i+"\n")
                 for j,k in recd[i].items():
-                    print(f"{j}: {k}")
+                    print(f"{j} {k}")
         elif todo == "4":
             if input("This will delete all the registered flights. Are you sure? ").lower()[0] == "y":
                 cleanRecord()
